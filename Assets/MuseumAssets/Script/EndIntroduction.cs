ff36a12bcef26b05214590d45bb8ae0ff67f2833
Federica Bucchieri <fede@mbp-di-federica.homenet.telecomitalia.it>
2021-01-28 11:46:24 +0100

    Added soundtracks, point of interest in the Museum Scene. Added EndGame scene

---
+++ Assets/MuseumAssets/Script/EndIntroduction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndIntroduction : MonoBehaviour
+{
+    public Animator EmiAnimator;
+    public string introduction;
+    public Text textBuble;
+    public float textTime = 0.02f;
+
+
+    public void Start()
+    {
+        //Play animation
+        EmiAnimator.SetTrigger("talking");
+
+        // activate the text to speech component
+        MagicRoomManager.instance.MagicRoomTextToSpeachManager.GenerateAudioFromText(introduction);
+
+        // starting to write text in Emi dialog bubble
+        textBuble.text = "";
+        StartCoroutine(EffectTypeWriter());
+
+        MagicRoomManager.instance.MagicRoomLightManager.SendColor(Color.yellow);
+        MagicRoomManager.instance.MagicRoomTextToSpeachManager.EndSpeak += StopTalking();
+
+    }
+
+    public System.Action StopTalking()
+    {
+        return BackToIdleEmi;
+    }
+
+    public void BackToIdleEmi()
+    {
+        //stopping Emi from talking
+        EmiAnimator.SetTrigger("idle");
+        MagicRoomManager.instance.MagicRoomLightManager.SendColor(Color.black);
+    }
+
+    private IEnumerator EffectTypeWriter()
+    {
+        foreach (char character in introduction.ToCharArray())
+        {
+            textBuble.text += character;
+            yield return new WaitForSeconds(textTime);
+        }
+
+        BackToIdleEmi();
+    }
+}
---
+++ Assets/OtherAssets/QuitButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void OnTriggerStay(Collider collision)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("ciao");
+            LevelLoader.startTransition("EndScene");
+        }
+
+    }
+}
